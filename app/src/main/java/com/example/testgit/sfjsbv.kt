823b7739461280f24594fed08e4354af900ef71f
lap2 <khanh1208206@gmail.com>
2026-04-15 11:20:56 +0700

    3

--- app/src/main/java/com/example/testgit/sfjsbv.kt
+++ app/src/main/java/com/example/testgit/sfjsbv.kt
@@ -1,4 +1,14 @@
 package com.example.testgit
 
+import androidx.compose.runtime.Composable
+import com.example.testgit.ui.theme.TestgitTheme
+
 class sfjsbv {
+
+}
+@Composable
+fun GreetingPrevie() {
+    TestgitTheme {
+        Greeting("Android")
+    }
 }
