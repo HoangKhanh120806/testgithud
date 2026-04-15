86ef73bf0be456faec53f5bfb1bfa7abbdacd5ab
lap2 <khanh1208206@gmail.com>
2026-04-15 11:30:10 +0700

    4

--- app/src/main/java/com/example/testgit/ui/theme/addd.kt
+++ app/src/main/java/com/example/testgit/ui/theme/addd.kt
@@ -1,4 +1,14 @@
 package com.example.testgit.ui.theme
 
+import androidx.compose.runtime.Composable
+import com.example.testgit.Greeting
+
 class addd {
+}
+
+@Composable
+fun GreetingPrevirte() {
+    TestgitTheme {
+        Greeting("Androizhfgsfgd")
+    }
 }
